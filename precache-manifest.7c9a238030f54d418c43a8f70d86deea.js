e6f18319d1110e329c06581fd853f4ca59abb52c
Kentaro91011 <kentaro.10.11@icloud.com>
2019-08-28 19:59:52 +0900

    Updates

---
+++ precache-manifest.7c9a238030f54d418c43a8f70d86deea.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c73714fd21d64be71ca1cccd093187e7",
+    "url": "/tinder_ui/index.html"
+  },
+  {
+    "revision": "09259a962a35efa3f0e6",
+    "url": "/tinder_ui/static/css/main.8914a4f2.chunk.css"
+  },
+  {
+    "revision": "ec553f4a323faad7a8e2",
+    "url": "/tinder_ui/static/js/2.8160ea33.chunk.js"
+  },
+  {
+    "revision": "09259a962a35efa3f0e6",
+    "url": "/tinder_ui/static/js/main.b30dc07c.chunk.js"
+  },
+  {
+    "revision": "58734a802e6025d21804",
+    "url": "/tinder_ui/static/js/runtime~main.a4ebf1c7.js"
+  },
+  {
+    "revision": "72c25d522d4dc19669509218910d5b37",
+    "url": "/tinder_ui/static/media/ayaka.72c25d52.jpg"
+  },
+  {
+    "revision": "edaf98d1e7cfa8a915363ed2a2d252b9",
+    "url": "/tinder_ui/static/media/fujita_nikoru.edaf98d1.jpg"
+  },
+  {
+    "revision": "d244ae8162d95c84d1356a758f78beff",
+    "url": "/tinder_ui/static/media/komatsu_nana.d244ae81.png"
+  },
+  {
+    "revision": "949a5f9e6e38c2421d08c230692baf9c",
+    "url": "/tinder_ui/static/media/yuko.949a5f9e.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a4b70998901ea910bdf78bf1db06ecf4.js"
+  "/tinder_ui/precache-manifest.7c9a238030f54d418c43a8f70d86deea.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tinder_ui/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
